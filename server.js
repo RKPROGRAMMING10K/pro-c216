bfaea5a225ca44240845e165dc3a37baa8eaa62e
rkprograming10 <rvn276@gmail.com>
2023-01-17 18:54:14 +0530

    video-chat

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { Socket } = require("socket.io");
+const app = express();
+
+const server = require("http").Server(app);
+
+const io = require('socket.io')(server, {cors: {origin: '*'}})
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+io.on("connection", (Socket) =>{
+    Socket.on("message", (message) => {
+        io.emit("createMessage", message);
+    });
+})
+
+// io.on("connection", (socket) => {
+//     socket.on("message", (message) => {
+//         io.emit("createMessage", message);
+//     });
+// });
+
+server.listen(3030);
